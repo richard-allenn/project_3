9ca1ff912f46a55188bc7aad4d16096b5e7abe71
Conor McCabe <mccabeconor941@gmail.com>
2023-04-17 16:57:33 +0800

    added files

--- app2.py
+++ app2.py
@@ -10,3 +10,61 @@ from dateutil.relativedelta import relativedelta
 
 from flask import Flask, jsonify
 
+#################################################
+# Database Setup
+#################################################
+
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/perth_crimes")
+
+print(engine.url)
+
+# reflect an existing database into a new model
+
+results = engine.execute("SELECT * FROM perth_crime_data")
+print(results)
+
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def get_data():
+    with engine.connect() as conn:
+        result = conn.execute('select * FROM perth_crime_data')
+        headers = result.keys()
+        rows = result.fetchall()
+        data = []
+        for row in rows:
+            data.append(dict(zip(headers, row)))
+
+        return jsonify(data)
+
+        
+@app.route("/api/v1.0/stations")
+def get_stations():
+    with engine.connect() as conn:
+        result = conn.execute('select * FROM station_data')
+        headers = result.keys()
+        rows = result.fetchall()
+        data = []
+        for row in rows:
+            data.append(dict(zip(headers, row)))
+
+        return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
---
+++ district.js
@@ -0,0 +1,33 @@
+// district map
+
+let myMap = L.map("map", {
+    center: [-32.8, 117.9],
+    zoom: 7
+  });
+  
+  // Adding the tile layer
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+  
+  var data = fetchJSON('Perth_data.geojson')
+            .then(function(data) { return data })
+  
+  d3.json(data).then(function(response) {
+  
+    //console.log(response);
+    features = response.features;
+  
+    //console.log(features);
+  
+    for (let i = 0; i < features.length; i++) {
+  
+      let location = features[i].geometry;
+      if(location){
+        L.marker([location.coordinates[1].y, location.coordinates[0].x]).addTo(myMap);
+      }
+    
+    }
+  
+  });
+  
